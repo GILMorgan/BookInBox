8f8e4b1d4a5929cc2e13a8a6c1bd7540ab7426d4
morgan <gilmorgan.perso@gmail.com>
2026-03-27 15:39:53 +0100

    Add entity and repository for user Base configuration for security Add auth form (some style needed) Add command to add user

---
+++ src/Command/BookinboxUserCreateCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Uid\Uuid;
+
+#[AsCommand(
+    name: 'bookinbox:user:create',
+    description: 'Add a new user in the database',
+)]
+class BookinboxUserCreateCommand extends Command
+{
+	public function __construct(
+		private readonly UserPasswordHasherInterface $userPasswordHasher,
+		private readonly UserRepository $userRepository
+	)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+			->addArgument('email', InputArgument::REQUIRED, 'User email')
+			->addArgument('password', InputArgument::REQUIRED, 'User password')
+			->addArgument('role', InputArgument::OPTIONAL, 'role, by default ROLE_USER')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+		$email = $input->getArgument('email');
+		$password = $input->getArgument('password');
+		$roles = ["ROLE_USER"];
+
+		if ($role = $input->getArgument('role')) {
+			$roles[] = $role;
+		}
+
+		$user = new User();
+		$user
+			->setId((string) Uuid::v4())
+			->setEmail($email)
+			->setRoles($roles)
+		;
+
+		$hashedPassword = $this->userPasswordHasher->hashPassword(
+            $user,
+            $password
+        );
+        $user->setPassword($hashedPassword);
+
+		$this->userRepository->save($user);
+
+        $io->success('The user as been successfully created.');
+
+        return Command::SUCCESS;
+    }
+}
