345fd57c4b55d2807e4d8e8e7040def1fd14d979
morgan <gilmorgan.perso@gmail.com>
2026-04-11 14:40:21 +0200

    Base book handling

--- tests/Providers/BookProviderTest.php
+++ tests/Providers/BookProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\Providers;
 
+use App\Entity\Book;
 use App\Providers\BookProvider;
 use App\Repository\BookRepository;
 use App\Services\Books\BookSerializer;
@@ -24,5 +25,32 @@ class BookProviderTest extends TestCase
         );
 
         $this->assertSame($dto, $bookProvider->save($dto));
+    }
+
+    public function testGetAll()
+    {
+        $dto = BookFactory::getBook();
+        $entity = new Book();
+        $entity
+            ->setId("1245-afdc-457ef-5f7fff")
+            ->setOpenLibraryId("OL45804W")
+            ->setTitle("title")
+            ->setAuthors(["authorId"])
+            ->setPublishDate("25/12/1978")
+            ->setPublisher("Pingouin editions")
+            ->setIsbn10("isbn10")
+            ->setIsbn13("isbn13")
+            ->setNumberOfPages(150)
+        ;
+
+        $bookRepository = Mockery::mock(BookRepository::class);
+        $bookRepository->shouldReceive("findAll")->andReturn([$entity]);
+
+        $bookProvider = new BookProvider(
+            $bookRepository,
+            new BookSerializer()
+        );
+
+        $this->assertCount(1, $bookProvider->getAll());
     }
 }
