b46bb4d43e298aab4fa6fdc9cc19e3dcd374c05d
morgan <gilmorgan.perso@gmail.com>
2026-05-12 15:14:31 +0200

    WIP

--- assets/vue/components/bookTable.vue
+++ assets/vue/components/bookTable.vue
@@ -1,19 +1,3 @@
-<style scoped>
-    nav {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-    }
-
-    nav a {
-        border: 1px solid black;
-        display: block;
-        padding: 8px 12px;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-</style>
-
 <template>
   <table>
     <thead>
@@ -30,23 +14,18 @@
     </tbody>
   </table>
 
-  <nav>
-    <a @click="prevLink()">&laquo;</a>
-    <div v-for="pageLink in pageLinks">
-        <a @click="goToLink(pageLink)">{{ pageLink }}</a>
-    </div>
-    <a @click="nextLink()">&raquo;</a>
-  </nav>
+  <paginator :url='url' :nbItems='nbItems' @update="handleUpdate"></paginator>
 </template>
 
 <script setup>
 import { ref, toRefs } from 'vue'
-import axios from 'axios' 
+import axios from 'axios'
 
 const books = ref([])
 const currentPage = ref(1)
 const maxPage = ref(1)
 const pageLinks = ref([])
+const nbItems = ref(0)
 
 const params = {
   page: currentPage.value,
@@ -60,39 +39,28 @@ const props = defineProps(
 
 const { url } = toRefs(props)
 
-const getBooks = function () {
+const handleUpdate = function (event) {
     const params = {
-        page: currentPage.value,
+        page: event.value,
     }
 
     axios
         .get(url.value, {params})
         .then(response => {
             books.value = response.data.books
-            pageLinks.value = totalBooks(response.data.nbBooks)
+            nbItems.value = response.data.nbBooks
         })
         .catch(error => {
             console.error('Error fetching data:', error)
-        });
+        })
 }
 
-const totalBooks = function (nbBooks) {
-    maxPage.value = Math.ceil(nbBooks / 25)
-    let pageLinks = []
-
-    for (var i = 0; i < maxPage.value; i++) {
-        pageLinks.push(i + 1)
-    }
-
-    return pageLinks
-}
-
-const nextLink = function () {
+/*const nextLink = function () {
     if (currentPage.value < maxPage.value) {
         currentPage.value++
         getBooks()
     }
-}
+}*/
 
 const prevLink = function () {
     if (currentPage.value > 1) {
@@ -128,9 +96,6 @@ const getName = function (book) {
 
     return "-"
 }
-
-getBooks()
-
 </script>
 
 <script>
